4d3ed3c7316d364ae620fba7e982b71c5dc87012
steeltitanunbrk <fortune18000@gmail.com>
2024-09-18 23:40:21 +0000

    adding of models to django admin site

--- project/admin.py
+++ project/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from project.models import CustomUser, Notification, Project, Task
+
 # Register your models here.
+
+# Adding models to django admin site
+admin.site.register(CustomUser)
+admin.site.register(Project)
+admin.site.register(Task)
+admin.site.register(Notification)
